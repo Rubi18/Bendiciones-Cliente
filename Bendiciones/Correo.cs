2379fa7b0cf9b9b16472d614e1c0dce894a55ff2
Antony Palacios <apalaciosc@pucp.pe>
2019-11-23 10:31:06 -0500

    Correo para clietne al matricularse ene un servicio

--- Bendiciones/Correo.cs
+++ Bendiciones/Correo.cs
@@ -36,30 +36,31 @@ namespace Bendiciones
 					MessageBox.Show("Se ha enviado el correo a " + colaborador.email, "", MessageBoxButtons.OK, MessageBoxIcon.Information);
 				}
 			}
-			//try
-   //         {
-   //             MailMessage mail = new MailMessage();
-   //             mail.From = new MailAddress(emailFromAddress);
-   //             mail.To.Add(colaborador.email);
-   //             mail.Subject = "Bienvenido a Escula para Embarazadas" + colaborador.nombre;
-   //             mail.Body = "Usuario: " + colaborador.user +
-   //                 "\nContraseña: " + colaborador.password + "\n Contraseña persola. Por favor, no comparta con nadie su contraseña";
-   //             mail.IsBodyHtml = true;
-
-   //             SmtpClient smtp = new SmtpClient(smtpAddress, portNumber);
-   //             smtp.Credentials = new NetworkCredential(emailFromAddress, password);
-   //             smtp.EnableSsl = enableSSL;
-   //             smtp.Send(mail);
-   //             MessageBox.Show("Se ha enviado el correo a "+colaborador.email , "", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-   //         }
-   //         catch(Exception ex)
-   //         {
-   //             MessageBox.Show(ex.Message,"",MessageBoxButtons.OK,MessageBoxIcon.Information);
-   //         }
                
             
         }
-    }
+
+		public void CorreoNuevoServicio(Service.colaborador cliente,Service.curso curso)
+		{
+
+			using (MailMessage mail = new MailMessage())
+			{
+				mail.From = new MailAddress(emailFromAddress);
+				mail.To.Add(cliente.email);
+				mail.Subject = "Bienvenida/o a Escuela para embarazadas " + cliente.nombre;
+				mail.Body = "El dia de hoy se matriculo en el curso "+curso.nombre;
+				mail.IsBodyHtml = true;
+				using (SmtpClient smtp = new SmtpClient(smtpAddress, portNumber))
+				{
+					smtp.Credentials = new NetworkCredential(emailFromAddress, password);
+					smtp.EnableSsl = enableSSL;
+					smtp.Send(mail);
+					MessageBox.Show("Se ha enviado el correo a " + cliente.email, "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				}
+			}
+
+
+		}
+	}
 }
 
